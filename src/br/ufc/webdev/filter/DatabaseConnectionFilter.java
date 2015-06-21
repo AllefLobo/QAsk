c7ed5188344f9e7b9bdb5056635011f8c04e515f
Allef Lobo <allef.lobo@gmail.com>
2015-06-21 09:27:56 -0300

    Adicao das classes de conexao com o banco de dados.

---
+++ src/br/ufc/webdev/filter/DatabaseConnectionFilter.java
@@ -0,0 +1,39 @@
+package br.ufc.webdev.filter;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import br.ufc.webdev.model.ConnectionFactory;
+
+public class DatabaseConnectionFilter implements Filter {
+
+	@Override
+	public void destroy() {
+
+	}
+ 
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp,
+			FilterChain chain) throws IOException, ServletException {
+
+		Connection connection = new ConnectionFactory().getConnection();
+		req.setAttribute("connection", connection);
+		chain.doFilter(req, resp);
+		try {connection.close();} catch (SQLException e) {}
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+
+	}
+
+}
