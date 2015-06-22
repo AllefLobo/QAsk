a86fe3143ffe33cd008c17b8935160b66df54f3e
Allef Lobo <allef.lobo@gmail.com>
2015-06-22 17:47:34 -0300

    updates

--- src/br/ufc/webdev/model/Pergunta.java
+++ src/br/ufc/webdev/model/Pergunta.java
@@ -3,9 +3,6 @@ package br.ufc.webdev.model;
 public class Pergunta {
 	private int id;
 	private String conteudo;
-	private int id_remetente;
-	private int id_destino;
-	private int id_resposta;
 	
 	public int getId() {
 		return id;
@@ -18,25 +15,6 @@ public class Pergunta {
 	}
 	public void setConteudo(String conteudo) {
 		this.conteudo = conteudo;
-	}
-	
-	public int getId_destino() {
-		return id_destino;
-	}
-	public void setId_destino(int id_destino) {
-		this.id_destino = id_destino;
-	}
-	public int getId_remetente() {
-		return id_remetente;
-	}
-	public void setId_remetente(int id_remetente) {
-		this.id_remetente = id_remetente;
-	}
-	public int getId_resposta() {
-		return id_resposta;
-	}
-	public void setId_resposta(int id_resposta) {
-		this.id_resposta = id_resposta;
 	}
 	
 	
