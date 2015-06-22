f9556d990aa373b2cde671972054cec553c3cb53
robson <robson@robson-cavalcante>
2015-06-22 20:12:04 -0300

    feferfe

--- src/br/ufc/webdev/controller/deleteController.java
+++ src/br/ufc/webdev/controller/deleteController.java
@@ -20,7 +20,7 @@ public class deleteController extends HttpServlet{
 	 */
 	private static final long serialVersionUID = 1L;
 
-	@Override
+	@Override //
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
