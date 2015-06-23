90881407a24321db0b54704cd2129eff4df9e15d
robson <robson@robson-cavalcante>
2015-06-23 15:04:50 -0300

    Adicao do ListController

--- src/br/ufc/webdev/controller/ListController.java
+++ src/br/ufc/webdev/controller/ListController.java
@@ -1,11 +1,18 @@
 package br.ufc.webdev.controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import br.ufc.webdev.model.Pessoa;
+import br.ufc.webdev.model.PessoaDAO;
 
 /**
  * Servlet implementation class ListController
@@ -13,27 +20,23 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ListarPessoa")
 public class ListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ListController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+  
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp){
 		// TODO Auto-generated method stub
+		
+		
+		Connection connection = (Connection) req.getAttribute("connection");
+		PessoaDAO dao = new PessoaDAO(connection);
+		try {
+			List<Pessoa> pessoas = dao.getAll();
+			req.setAttribute("pessoa", pessoas);
+			req.getRequestDispatcher("index.jsp").forward(req, resp);
+			
+		} catch (SQLException | ServletException | IOException e) {}
+	
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
+		
 }
+
+
